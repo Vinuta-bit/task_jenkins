1d8d17b16706aabdab2c317d0fb28f5ed0616e43
Vinuta-bit <vinutasinnur22@gmail.com>
2024-12-17 09:13:03 +0530

    Update script1.sh

---
+++ script1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir dir1 dir2 dir3
+
+touch dir1/file1.txt dir1/file2.txt dir1/file3.txt
+
+touch dir2/file1.txt dir2/file2.txt dir2/file3.txt
+
+touch dir3/file1.txt dir3/file2.txt dir3/file3.txt
